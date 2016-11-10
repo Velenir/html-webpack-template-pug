f4ff58f511ebce6601a03cfe835284a964d1a587
Velenir <Velenir@users.noreply.github.com>
2016-11-10 22:27:10 +0300

    Added example for extracting css from separate entry in production

---
+++ examples/minimalProd-extractCSS-separateEntry/app/index.js
@@ -0,0 +1,6 @@
+// index.js
+console.log("index.js");
+
+const app = document.getElementById('app');
+
+app.textContent = "This element was dynamically filled by a script.";
---
+++ examples/minimalProd-extractCSS-separateEntry/webpack.config.js
@@ -0,0 +1,61 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: './app',
+		style: './app/main.css'
+	},
+	output: {
+		path: './build',
+		filename: '[name].js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract('style', 'css')
+			},
+			{
+				test: /\.pug$/,
+				loader: 'pug'
+			}
+		]
+	},
+	// only needed to resolve local version for testing
+	// in your code install properly as a package
+	resolve: {
+		alias: {
+			"html-webpack-template-pug": "../../"
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: false,
+			template: 'index.pug',
+			excludeJSChunks: 'style',	// don't include specific chunks in scripts (when .js is a byproduct of already extracted .css)
+			appMountId: 'app',
+			mobile: true,
+			title: 'App',
+			injectExtras: {
+				head: [
+					"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css",
+					{
+						"tag": "meta",
+						"name": "description",
+						"content": "A description of the page"
+					}
+				],
+				body: [
+					"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js",
+					"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js",
+					{
+						"tag": "noscript",
+						"innerHTML": "JavaScript is disabled in your browser. <a href='http://www.enable-javascript.com/' target='_blank'>Here</a> is how to enable it."
+					}
+				]
+			}
+		}),
+		new ExtractTextPlugin('[name].css')
+	]
+};
